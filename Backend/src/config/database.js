b50e9f763e19853497261d6b506a2c0d7debcff0
Mukesh-Ghildiyal <mukeshghildiyal435@gmail.com>
2025-07-30 14:53:40 +0530

    resolve >>mongouri error

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
-    const mongoUri = process.env.MONGODB_URI
+    const mongoUri = process.env.MONGODB_URI;
 
-=======
-    const mongoUri = process.env.MONGODB_URI ;
-    
->>>>>>> e795cc0e79f002d6b7c886ed0e214bf9c145524b
     await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
